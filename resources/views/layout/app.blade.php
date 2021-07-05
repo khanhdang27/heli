20b7b4be4907dd82aef46a21efb5bc8bc1e07321
Bành Phước Trung <trungbanh.mkd@gmail.com>
2021-07-05 17:40:05 +0700

    app chat edit

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -39,35 +39,35 @@
 
     @include('layout.footer')
     <script>
-        var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?26718';
+        var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?37547';
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
         s.src = url;
         var options = {
-            "enabled":true,
-            "chatButtonSetting":{
-                "backgroundColor":"#0b487d",
-                "ctaText":"",
-                "borderRadius":"25",
-                "marginLeft":"0",
-                "marginBottom":"50",
-                "marginRight":"50",
-                "position":"right"
-            },
-            "brandSetting":{
-                "brandName":"Helios",
-                "brandSubTitle":"",
-                "brandImg":"",
-                "welcomeText":"Hi there!\nHow can I help you?",
-                "messageText":"Hello, I have a question",
-                "backgroundColor":"#0b487d",
-                "ctaText":"Start Chat",
-                "borderRadius":"25",
-                "autoShow":false,
-                "phoneNumber":"85292296725"
-            }
-        };
+      "enabled":true,
+      "chatButtonSetting":{
+          "backgroundColor":"#0b487d",
+          "ctaText":"",
+          "borderRadius":"25",
+          "marginLeft":"0",
+          "marginBottom":"50",
+          "marginRight":"50",
+          "position":"right"
+      },
+      "brandSetting":{
+          "brandName":"Helios",
+          "brandSubTitle":"",
+          "brandImg":"https://project.nironcrm.tk/helios/public/images/logo_chat.png",
+          "welcomeText":"Hi there!\nHow can I help you?",
+          "messageText":"Hello, I have a question about {{page_link}}",
+          "backgroundColor":"#e6c06c",
+          "ctaText":"Start Chat",
+          "borderRadius":"25",
+          "autoShow":false,
+          "phoneNumber":"85292296725"
+      }
+    };
         s.onload = function() {
             CreateWhatsappChatWidget(options);
         };
