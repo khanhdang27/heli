67c97c87127489094eeb4bac2b6ad9585c288bd6
Banh Trung <trungbanh97@gmail.com>
2021-08-18 13:37:03 +0700

    update animate

--- resources/views/components/home/video-course.blade.php
+++ resources/views/components/home/video-course.blade.php
@@ -1,36 +1,34 @@
 @php
 
-    $lecture_default = null;
+$lecture_default = null;
 
-    $defaultSource = '';
-    $list_lecture = $courseDetail->lecture;
+$defaultSource = '';
+$list_lecture = $courseDetail->lecture;
 
-    if (empty($latesLecture)) {
-        $lecture_default = $list_lecture->first();
+if (empty($latesLecture)) {
+$lecture_default = $list_lecture->first();
 
-    } else {
-        $lecture_default = $courseDetail->lecture->first(function($item) use ($latesLecture) {
-                    return $item->id == $latesLecture;
-                });
-    }
+} else {
+$lecture_default = $courseDetail->lecture->first(function($item) use ($latesLecture) {
+return $item->id == $latesLecture;
+});
+}
 
-    if (!empty($lecture_default)) {
-        $defaultSource = 'https://player.vimeo.com/video/' . $lecture_default->video_resource . '?title=0&amp;byline=0&amp;portrait=0&amp;speed=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id='.config('vimeo_app_id');
-    }
+if (!empty($lecture_default)) {
+$defaultSource = 'https://player.vimeo.com/video/' . $lecture_default->video_resource .
+'?title=0&amp;byline=0&amp;portrait=0&amp;speed=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id='.config('vimeo_app_id');
+}
 
 
-    $is_bought = false;
-    $student_courses = null;
+$is_bought = false;
+$student_courses = null;
 
-    if (Auth::check() && !empty(Auth::user()->student_courses())) {
-        if (Auth::user()->student_courses()->get()->count()) {
-
-            $student_courses = Auth::user()->student_courses()->get();
-
-            $is_bought = $student_courses->firstWhere('course_id', $lecture_default->course_id);
-
-        }
-    }
+if (Auth::check() && !empty(Auth::user()->student_courses())) {
+if (Auth::user()->student_courses()->get()->count()) {
+$student_courses = Auth::user()->student_courses()->get();
+$is_bought = $student_courses->firstWhere('course_id', $lecture_default->course_id);
+}
+}
 
 @endphp
 
@@ -41,73 +39,67 @@
                 <div class="h2 mb-0 mr-3">{{ $courseDetail->course_description }}</div>
                 <div class="mt-auto">
                     @if (Auth::check())
-                        <x-like.like :likeRef=$courseDetail :likeModule=\App\Models\Course::class></x-like.like>
+                    <x-like.like :likeRef=$courseDetail :likeModule=\App\Models\Course::class></x-like.like>
                     @endif
                 </div>
                 {{--            <div class="h2 m-0">{{ $courseDetail->lecture->first(function($item) use ($lecture_default) {--}}
                 {{--                return $item->id == $lecture_default->id;--}}
-                {{--            })->lectures_name }}</div>--}}
-            </div>
-            <p class="h5 mt-5 text-primary">{{ $courseDetail->tutor->full_name }}</p>
-            <div class="d-flex flex-sm-wrap top-course-detail justify-content-between mb-5">
-                <div class="d-flex align-items-center text-primary">
-                    @php
-                        $rate = (int)floor($courseDetail->rating_average);
-                    @endphp
-                    @for ($i = 0; $i < $rate; $i++)
-                        <img src="{{ asset('images/ic/ic_star.svg') }}" width="29">
-                    @endfor
-                    @for ($i = 0; $i < 5-$rate; $i++)
-                        <img src="{{ asset('images/ic/ic_star_border.svg') }}" width="29">
-                    @endfor
-                    <h5 class="mb-0 ml-3">{{$courseDetail->rating_average}}/5</h5>
-                </div>
-            </div>
+                {{--            })->lectures_name }}
+            </div>--}}
         </div>
-        <div class="col-lg-4">
-
+        <p class="h5 mt-5 text-primary">{{ $courseDetail->tutor->full_name }}</p>
+        <div class="d-flex flex-sm-wrap top-course-detail justify-content-between mb-5">
+            <div class="d-flex align-items-center text-primary">
+                @php
+                $rate = (int)floor($courseDetail->rating_average);
+                @endphp
+                @for ($i = 0; $i < $rate; $i++) <img src="{{ asset('images/ic/ic_star.svg') }}" width="29">
+                    @endfor
+                    @for ($i = 0; $i < 5-$rate; $i++) <img src="{{ asset('images/ic/ic_star_border.svg') }}" width="29">
+                        @endfor
+                        <h5 class="mb-0 ml-3">{{$courseDetail->rating_average}}/5</h5>
+            </div>
         </div>
     </div>
-    <div class="row mb-4" id="video-lecture">
-        <div class="col-lg-8 bg-white">
-            <div class="embed-responsive embed-responsive-16by9">
-                <iframe id="videoView" src={{ $defaultSource }} class="embed-responsive-item" frameborder="0"
-                        allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="Default name"></iframe>
-            </div>
-        </div>
-        <div class="col-lg-4 bg-white">
-            <div class="box-list-video text-primary pt-3">
-                <ol>
-                    @foreach ($list_lecture as $index => $item)
-                        @if ($index == 0)
-                            <li class="lecture-active h2" role="button" v-on:click="clickLecture"
-                                data-id="{{ $item->video_resource }}"
-                                data-lecture={{ $item->id }}>
-                                {{ $item->lectures_name }}
-                            </li>
-                        @else
-                            <li class="lecture-{{ $is_bought ? 'active' : 'inactive'}} h2"
-                                role="button"
-                                v-on:click="clickLecture"
-                                data-id="{{  $is_bought  ? $item->video_resource : null }}"
-                                data-lecture={{ $item->id }} >
-                                {{ $item->lectures_name }}
-                            </li>
-                        @endif
-                    @endforeach
-                </ol>
-            </div>
-            @if(!Auth::check())
-                <div class="text-center btn-above-video mt-5">
-                    <button class="btn-register-now mt-0" id="" data-toggle="modal"
-                            data-target="#registerModal">
-                        <h4 class="mb-0 font-weight-bold">@lang('keywords.tryItNow')</h4>
-                    </button>
-                </div>
-            @endif
+    <div class="col-lg-4">
 
+    </div>
+</div>
+<div class="row mb-4" id="video-lecture">
+    <div class="col-lg-8 bg-white">
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe id="videoView" src={{ $defaultSource }} class="embed-responsive-item" frameborder="0"
+                allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="Default name"></iframe>
         </div>
     </div>
+    <div class="col-lg-4 bg-white">
+        <div class="box-list-video text-primary pt-3">
+            <ol>
+                @foreach ($list_lecture as $index => $item)
+                @if ($index == 0)
+                <li class="lecture-active h2" role="button" v-on:click="clickLecture"
+                    data-id="{{ $item->video_resource }}" data-lecture={{ $item->id }}>
+                    {{ $item->lectures_name }}
+                </li>
+                @else
+                <li class="lecture-{{ $is_bought ? 'active' : 'inactive'}} h2" role="button" v-on:click="clickLecture"
+                    data-id="{{  $is_bought  ? $item->video_resource : null }}" data-lecture={{ $item->id }}>
+                    {{ $item->lectures_name }}
+                </li>
+                @endif
+                @endforeach
+            </ol>
+        </div>
+        @if(!Auth::check())
+        <div class="text-center btn-above-video mt-5">
+            <button class="btn-register-now mt-0" id="" data-toggle="modal" data-target="#registerModal">
+                <h4 class="mb-0 font-weight-bold">@lang('keywords.tryItNow')</h4>
+            </button>
+        </div>
+        @endif
+
+    </div>
+</div>
 </div>
 
 <script>
--- resources/views/home/home-page.blade.php
+++ resources/views/home/home-page.blade.php
@@ -3,122 +3,158 @@
 @section('title', 'Home Page')
 
 @section('content')
-    <div class="banner">
-        <x-sub-header :subjects=$subjects page="home"></x-sub-header>
-        @if ($message = Session::get('error'))
-            <div class="alert alert-danger alert-block">
-                <button type="button" class="close" data-dismiss="alert">×</button>
-                <strong>{{ $message }}</strong>
-            </div>
-        @endif
-        @if (!Auth::check())
-            <x-home.banner :banner=$banners></x-home.banner>
-        @else
-            <br>
-            <div class="container pt-5 mt-5">
-                <h1 class="text-center text-secondary">
-                    @lang('keywords.latestNewsPage')
-                </h1>
-                <hr class="border-secondary">
-                <div class="mx-auto box-news">
-                    @foreach ($news as $item)
-                        <div class=" my-2">
-                            <a class="h2" href="{{ route('site.news-detail', $item->id) }}">
-                                {{ $item->announcement_date }}
-                                {{ $item->title }}
-                            </a><br>
-                        </div>
-
-                    @endforeach
-                </div>
-            </div>
-            <!-- #endregion -->
-        @endif
+<div class="banner">
+    <x-sub-header :subjects=$subjects page="home"></x-sub-header>
+    @if ($message = Session::get('error'))
+    <div class="alert alert-danger alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
     </div>
-    <div class="body-content">
-        <div class="container-fluid container-home mt-5">
-            <div class="row">
-                @if(!Auth::check())
-                    <div class="col-lg-5 col-12">
-                        <h2 class="text-primary font-weight-bold">
-                            @lang('keywords.latestDiscountProduct')
-                        </h2>
-                        <x-product.course-list :courses=$course_latest typeOfUI="lasted"></x-product.course-list>
-                    </div>
-                    <div class="col-lg-2"></div>
-                    <div class="col-lg-5 col-12 welcome-offer">
-                        <h2 class="text-primary font-weight-bold">
-                            @lang('keywords.welcomeOffer')
-                        </h2>
-                        <x-product.course-list :courses=$course_welcomes typeOfUI="welcome"></x-product.course-list>
-                    </div>
-                @else
-                    <div class="col-lg-6 col-12 welcome-offer">
-                        <h2 class="text-primary font-weight-bold">
-                            @lang('keywords.welcomeOffer')
-                        </h2>
-                        <x-product.course-list :courses=$course_welcomes typeOfUI="welcome"></x-product.course-list>
-                    </div>
-                @endif
-
+    @endif
+    @if (!Auth::check())
+    <x-home.banner :banner=$banners></x-home.banner>
+    @else
+    <br>
+    <div class="container pt-5 mt-5">
+        <h1 class="text-center text-secondary">
+            @lang('keywords.latestNewsPage')
+        </h1>
+        <hr class="border-secondary">
+        <div class="mx-auto box-news">
+            @foreach ($news as $item)
+            <div class=" my-2">
+                <a class="h2" href="{{ route('site.news-detail', $item->id) }}">
+                    {{ $item->announcement_date }}
+                    {{ $item->title }}
+                </a><br>
             </div>
+
+            @endforeach
         </div>
-        @if (Auth::check())
-            <div class="container-fluid container-home">
-            <h2 class="text-primary font-weight-bold" id="tab-title">
-                @lang('keywords.recommendedForYou')...
-            </h2>
-            <x-product.course-list :courses=$course_recommended typeOfUI="recommended"></x-product.course-list>
+    </div>
+    <!-- #endregion -->
+    @endif
+</div>
+<div class="body-content">
+    <div class="container-fluid container-home mt-5" id="body-content">
+        <div class="row">
+            @if(!Auth::check())
+            <div class="col-lg-5 col-12">
+                <h2 class="text-primary font-weight-bold">
+                    @lang('keywords.latestDiscountProduct')
+                </h2>
+                <x-product.course-list :courses=$course_latest typeOfUI="lasted"></x-product.course-list>
+            </div>
+            <div class="col-lg-2"></div>
+            <div class="col-lg-5 col-12 welcome-offer">
+                <h2 class="text-primary font-weight-bold">
+                    @lang('keywords.welcomeOffer')
+                </h2>
+                <x-product.course-list :courses=$course_welcomes typeOfUI="welcome"></x-product.course-list>
+            </div>
+            @else
+            <div class="col-lg-6 col-12 welcome-offer">
+                <h2 class="text-primary font-weight-bold">
+                    @lang('keywords.welcomeOffer')
+                </h2>
+                <x-product.course-list :courses=$course_welcomes typeOfUI="welcome"></x-product.course-list>
+            </div>
+            @endif
+
+        </div>
+    </div>
+    @if (Auth::check())
+    <div class="container-fluid container-home">
+        <h2 class="text-primary font-weight-bold" id="tab-title">
+            @lang('keywords.recommendedForYou')...
+        </h2>
+        <x-product.course-list :courses=$course_recommended typeOfUI="recommended"></x-product.course-list>
     </div>
     @endif
     <div class="product-tab">
         <x-home.product-tab :courseIGCSE=$courseIGCSE :courseUKISET=$courseUKISET :courseIELTS=$courseIELTS
-                            :courseIAL=$courseIAL typeOfUI="hot"></x-home.product-tab>
+            :courseIAL=$courseIAL typeOfUI="hot"></x-home.product-tab>
     </div>
     @if (!Auth::check())
-        <x-home.step-register></x-home.step-register>
+    <x-home.step-register></x-home.step-register>
     @endif
     @if (!Auth::check())
-        {{-- chua dang nhap --}}
-        <x-home.video-course :courseDetail=$courseVideo></x-home.video-course>
+    {{-- chua dang nhap --}}
+    <x-home.video-course :courseDetail=$courseVideo></x-home.video-course>
 
     @else
-        {{-- da dang nhap --}}
-        <div class="mx-auto container-home mt-5">
-            <div class="d-flex justify-content-between flex-wrap pt-5 btn-above-video">
-                <h2 class="text-primary">@lang('keywords.continueMyCourse')</h2>
-                <a href="{{ route('site.user.course') }}"
-                   class="m-0 btn btn-register-now btn-login py-1 px-5">
-                    @lang('keywords.otherPurchasedCourses')
-                </a>
-            </div>
-            <x-home.video-course :courseDetail=$courseVideo :latesLecture=$latesLecture></x-home.video-course>
+    {{-- da dang nhap --}}
+    <div class="mx-auto container-home mt-5">
+        <div class="d-flex justify-content-between flex-wrap pt-5 btn-above-video">
+            <h2 class="text-primary">@lang('keywords.continueMyCourse')</h2>
+            <a href="{{ route('site.user.course') }}" class="m-0 btn btn-register-now btn-login py-1 px-5">
+                @lang('keywords.otherPurchasedCourses')
+            </a>
         </div>
+        <x-home.video-course :courseDetail=$courseVideo :latesLecture=$latesLecture></x-home.video-course>
+    </div>
     @endif
     @if (!Auth::check())
-        <div class="free-class-container">
-            <div class="position-relative d-flex align-items-center">
-                <div class="border-right-radius border-primary">
-                    <div class="text-content d-flex justify-content-end text-primary">
-                        <h1 class="text-information text-content mb-0">
-                            @lang('keywords.freeTrialClass')
-                        </h1>
-                    </div>
-                </div>
-                <div class="circle-check position-absolute bg-primary d-flex justify-content-center align-items-center">
-                    <img width="111px" src="{{ asset('images/ic/Group8.png') }}">
+    <div class="free-class-container" id="free-class">
+        <div class="position-relative d-flex align-items-center">
+            <div class="border-right-radius border-primary" id="border-right-radius">
+                <div class="text-content d-flex justify-content-end text-primary">
+                    <h1 class="text-information text-content mb-0">
+                        @lang('keywords.freeTrialClass')
+                    </h1>
                 </div>
             </div>
-            <div class="d-flex justify-content-center btn-above-video">
-                <button class="btn-register-now py-1" data-toggle="modal" data-target="#registerModalComponent">
-                    <span class="font-weight-bold">@lang('keywords.registerNowToExperience')</span>
-                </button>
-                <x-login.register-modal></x-login.register-modal>
+            <div class="circle-check position-absolute bg-primary d-flex justify-content-center align-items-center"
+                id="circle-check">
+                <img width="111px" src="{{ asset('images/ic/Group8.png') }}">
             </div>
         </div>
-        <x-home.review></x-home.review>
+        <div class="d-flex justify-content-center btn-above-video">
+            <button class="btn-register-now py-1" id="btn-free-class" data-toggle="modal"
+                data-target="#registerModalComponent">
+                <span class="font-weight-bold">@lang('keywords.registerNowToExperience')</span>
+            </button>
+            <x-login.register-modal></x-login.register-modal>
+        </div>
+    </div>
+    <script>
+        if (!String.prototype.format) {
+            String.prototype.format = function(...args) {
+                return this.replace(/(\{\d+\})/g, function(a) {
+                return args[+(a.substr(1, a.length - 2)) || 0];
+                });
+            };
+        }
+        const freeClass = popmotion.styler(document.getElementById('border-right-radius'));
+        const circleCheck = popmotion.styler(document.getElementById('circle-check'));
+        const btnFreeClass = popmotion.styler(document.getElementById('btn-free-class'));
+
+        const animationFreeClass = popmotion.tween({
+            from: -1200,
+            to: {
+                x: 0
+            },
+            duration: 1500
+        });
+        let is_scoll_into = false;
+        $(window).scroll(function() {
+            var hT = $('#free-class').offset().top,
+                hH = $('#free-class').outerHeight(),
+                wH = $(window).height(),
+                wS = $(this).scrollTop();
+                if (wS > (hT - hH) && wS < (hT + hH ) && !is_scoll_into){
+                    is_scoll_into = true;
+                    animationFreeClass.start(freeClass.set);
+                    animationFreeClass.start(circleCheck.set);
+                    animationFreeClass.start(btnFreeClass.set);
+            } 
+        });
+
+    </script>
+    <x-home.review></x-home.review>
     @endif
     <x-subscribe-container></x-subscribe-container>
-    </div>
+</div>
+
 
 @endsection
