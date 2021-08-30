7455026b1129eea06bb0acb0418f27447a423509
phung <phungle.mkd@gmail.com>
2021-08-30 17:50:12 +0700

    complete header, fix modal not display, fix banner

--- resources/views/components/home/banner.blade.php
+++ resources/views/components/home/banner.blade.php
@@ -1,10 +1,7 @@
 <div class="banner-content">
-    @if(!empty($banner->banner_background))
-        <img class="img-thumbnail border-0 p-0 rounded-0 banner-background"
-             src="{{ asset('/file/'.$banner->banner_background ?? 0)}}">
-    @else
-        <img class="img-thumbnail border-0 p-0 rounded-0 banner-background" src="{{asset('images/banner.jpg')}}">
-    @endif
+    <div class="banner-background"
+         style="background-image: url('{{ empty($banner->image) ? asset('images/banner.jpg') : asset('/file/'.$banner->image->id)}}')">
+    </div>
     <div class="bg-opacity"></div>
     <div class="banner-info">
         <div class="banner-caption text-white">
--- resources/views/components/sub-header.blade.php
+++ resources/views/components/sub-header.blade.php
@@ -14,49 +14,47 @@
    }
 
 @endphp
-<nav class="navbar navbar-expand-sm container-fluid py-3 px-5 position-absolute {{ $page != 'home' ? "bg-primary" : "sub-header" }}">
-    <button class="navbar-toggler btn-collapse" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-        <span><img src="{{asset("images/ic/ic-collapse.png")}}" width="40"></span>
-    </button>
-    <div class="collapse navbar-collapse flex-wrap" id="navbarTogglerDemo01">
-        <ul class="navbar-nav {{ $page != 'home' ? "mx-auto" : null }} mt-2 mt-lg-0">
-            <li class="nav-item dropdown">
+<nav class="nav py-3 px-5 position-absolute {{ $page != 'home' ? "bg-primary" : "sub-header" }} w-100 flex-md-nowrap flex-wrap-reverse">
+{{--    <button class="navbar-toggler btn-collapse" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"--}}
+{{--            aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">--}}
+{{--        <span><img src="{{asset("images/ic/ic-collapse.png")}}" width="40"></span>--}}
+{{--    </button>--}}
+    <ul class="nav {{ $page != 'home' ? "mx-auto" : null }} mt-2 mt-lg-0">
+        <li class="nav-item dropdown">
+            <a class="h5 mb-0 nav-link dropdown-toggle text-white" href="#" id="navbarCer" data-toggle="dropdown" >
+                {{ SelectionByClass::getValues(\App\Models\Certificate::class, 'certificate_code', 'id')[$default_certificate] }}
+            </a>
 
-                <a class="h5 mb-0 nav-link dropdown-toggle text-white" href="#" id="navbarCer" data-toggle="dropdown" >
-                    {{ SelectionByClass::getValues(\App\Models\Certificate::class, 'certificate_code', 'id')[$default_certificate] }}
-                </a>
-
-                <div class="dropdown-menu bg-primary border border-white rounded-0 dropdown-menu-language">
-                    @foreach (SelectionByClass::getValues(\App\Models\Certificate::class, 'certificate_code', 'id') as $key => $value)
-                        @if ($key != 1)
-                            @if ($page != 'home')
+            <div class="dropdown-menu bg-primary border border-white rounded-0 dropdown-menu-language">
+                @foreach (SelectionByClass::getValues(\App\Models\Certificate::class, 'certificate_code', 'id') as $key => $value)
+                    @if ($key != 1)
+                        @if ($page != 'home')
                             <a class="dropdown-item" href="{{ route($url, $cer_show). '?certificate='.$key }}"> {{$value}}</a>
-                            @else
+                        @else
                             <a class="dropdown-item" href="{{ route($url, ['certificate'=>$key]) }}"> {{$value}}</a>
-                            @endif
                         @endif
-                    @endforeach
-                </div>
-            </li>
-            @foreach ($subjects as $item)
+                    @endif
+                @endforeach
+            </div>
+        </li>
+        @foreach ($subjects as $item)
                 @if ($item->certificate_id != 1)
-                    <li class="nav-item active d-flex align-items-center flex-wrap px-3">
+                    <li class="nav-item active d-flex align-items-center flex-wrap">
                         <a href="{{ route('site.subject.show', $item->id) }}"
                            class="text-white nav-link"> {{ $item->subject_name }} </a>
                     </li>
                 @endif
             @endforeach
-        </ul>
-        <form class="{{ $page == 'home' ? "ml-auto" : null }} form-inline my-2 my-lg-0"
-              action="{{ route('site.course.search') }}">
-            <div class="search">
-                <span class="ic-cart"><img src="{{asset("images/ic/ic_search.svg")}}" width="26"></span>
-                <input name="course" class="ip-search" type="text"
-                       placeholder="@lang('keywords.footer.searchForCourses')">
-            </div>
-        </form>
-    </div>
+
+    </ul>
+    <form class="{{ $page == 'home' ? "ml-auto" : null }} form-inline my-lg-0"
+          action="{{ route('site.course.search') }}">
+        <div class="search">
+            <span class="ic-cart"><img src="{{asset("images/ic/ic_search.svg")}}" width="26"></span>
+            <input name="course" class="ip-search" type="text"
+                   placeholder="@lang('keywords.footer.searchForCourses')">
+        </div>
+    </form>
 </nav>
 
 
