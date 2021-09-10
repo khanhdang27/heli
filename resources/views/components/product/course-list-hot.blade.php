c034a6041340a561a42f6a4b8465a6880d1640a8
phung <phungle.mkd@gmail.com>
2021-09-10 18:52:24 +0700

    fix product tab

--- resources/views/components/product/course-list-hot.blade.php
+++ resources/views/components/product/course-list-hot.blade.php
@@ -1,39 +1,14 @@
-{{--<div class="pb-5 row">--}}
-{{--    @if (false)--}}
-{{--     neu la page 1 thi hidden--}}
-{{--        <div class="col-1 align-self-center">--}}
-{{--            <button class="btn btn-outline-primary rounded-circle border-2 border-primary animate-change-color py-auto">--}}
-{{--                <h1 class="text-center p-3 font-weight-bold my-auto"> ﹤ </h1>--}}
-{{--            </button>--}}
-{{--        </div>--}}
-{{--    @endif--}}
-{{--    <div class="col-10 row product-category-row">--}}
-{{--        @foreach($courses as $value)--}}
-{{--        <x-product.course-item :course=$value typeOfUI={{$typeOfUI}}>--}}
-{{--        </x-product.course-item>--}}
-{{--        @endforeach--}}
-{{--    </div>--}}
-{{--    @if (true)--}}
-{{--     neu la page 1 thi show to padding--}}
-{{--    <div class="col-1"></div>--}}
-{{--    @endif--}}
-{{--    <div class="col-1 align-self-center">--}}
-{{--        <button class="btn btn-outline-primary rounded-circle border-2 border-primary animate-change-color py-auto">--}}
-{{--            <h1 class="text-center p-3 font-weight-bold my-auto"> ﹥ </h1>--}}
-{{--        </button>--}}
-{{--    </div>--}}
-{{--</div>--}}
 @if(!$courses->isEmpty())
     <div class="pb-3 row flex-nowrap">
         <div class="col-1">
-            <div class="swiper-button-prev btn-prev" id="btn_prev">
+            <div class="swiper-button-prev btn-prev btn-prev{{$tab}}" id="btn_prev">
                 <div class="rounded-circle border-btn-next animate-change-color py-3 px-4">
                     <p class="h2 text-center mx-2">❮</p>
                 </div>
             </div>
         </div>
         <div class="px-0 col-10">
-            <div class="swiper-container" id="swiperCourse">
+            <div class="swiper-container" id="swiperCourse{{$tab}}">
                 <div class="swiper-wrapper pb-3">
                     @foreach($courses as $value)
                         <div class="swiper-slide mb-5">
@@ -47,7 +22,7 @@
 
         </div>
         <div class="col-1 d-none d-md-block">
-            <div class="swiper-button-next btn-next" id="btn_next">
+            <div class="swiper-button-next btn-next btn-next{{$tab}}" id="btn_next">
                 <div class="rounded-circle border-btn-next animate-change-color py-2 px-4">
                     <p class="m-0 h2 text-center">❯</p>
                     <p class="text-nowrap text-center m-0">更多</p>
@@ -58,15 +33,15 @@
     </div>
 
     <script>
-        var swiperCourseHot = new Swiper("#swiperCourse", {
+        var swiperCourseHot{{$tab}} = new Swiper("#swiperCourse{{$tab}}", {
             @if(Auth::check())
             slidesPerColumn: 2,
             slidesPerColumnFill: 'row',
             @endif
             speed: 700,
             navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
+                nextEl: ".btn-next{{$tab}}",
+                prevEl: ".btn-prev{{$tab}}",
             },
             pagination: {
                 el: "#coursePagination",
@@ -90,6 +65,8 @@
                     spaceBetween: 20,
                 }
             },
+            observer: true,
+            observeParents: true
         });
     </script>
 @endif
